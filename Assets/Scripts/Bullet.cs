1d6c0627179fe507c5ec041eaa560b81d51cd133
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-03-22 18:50:31 -0700

    Update Bullet.cs

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
-  public float speed = 5;
-  public float lifeTime = 5;
-  private Rigidbody rb;
+    public float speed = 5;
+    public float lifeTime = 5;
+    private Rigidbody rb;
 
-  void Awake()
-  {
-    rb = GetComponent<Rigidbody>();
-  }
-  void FixedUpdate()
-  {
-    rb.velocity = Vector3.forward * speed;
-    lifeTime -= Time.deltaTime;
-    Debug.Log(lifeTime);
-    if (lifeTime < 0) Destroy(gameObject);
-  }
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = Vector3.forward * speed;
+        lifeTime -= Time.deltaTime;
+        Debug.Log(lifeTime);
+        if (lifeTime < 0) 
+            Destroy(gameObject);
+    }
 }
