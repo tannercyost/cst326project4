0c2e8f46eee9d8c6f1ae5cd9cbe85b888afc5c6a
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-03-22 18:49:37 -0700

    Update WeaponFSM.cs

--- Assets/Scripts/WeaponFSM.cs
+++ Assets/Scripts/WeaponFSM.cs
@@ -4,45 +4,43 @@ using UnityEngine;
 
 public enum WeaponStates
 {
-  Idle,
-  Target,
-  Shoot
+    Idle,
+    Target,
+    Shoot
 }
 
 public enum WeaponParameters
 {
-  isTarget,
-  targetDistroyed,
-  newTarget
+    isTarget,
+    targetDistroyed,
+    newTarget
 }
-
 
 [RequireComponent(typeof(Animator))]
 public class WeaponFSM : MonoBehaviour
 {
-  private Animator weapon;
-  public GameObject bullet;
-  public GameObject target;
-  void Awake()
-  {
-    weapon = GetComponent<Animator>();
-  }
-  void Update()
-  {
-    if (Input.GetKeyDown(KeyCode.A))
+    private Animator weapon;
+    public GameObject bullet;
+    public GameObject target;
+    void Awake()
     {
-      weapon.SetBool(WeaponParameters.isTarget.ToString(), !weapon.GetBool(WeaponParameters.isTarget.ToString()));
+        weapon = GetComponent<Animator>();
     }
 
-    transform.LookAt(target.transform);
-    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            weapon.SetBool(WeaponParameters.isTarget.ToString(), !weapon.GetBool(WeaponParameters.isTarget.ToString()));
+        }
 
-  }
+        transform.LookAt(target.transform);
+    }
 
-  void Bang()
-  {
-    Debug.Log("Pow");
-    GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
-  }
+    void Bang()
+    {
+        Debug.Log("Pow");
+        GameObject newBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+    }
 
 }
