d7393927fdd6b19183f17aa8209d6d6cfa2deb57
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-03-29 20:26:22 -0700

    Implemented health bars.
    Still need to work on getting health bars to display actual health values.

---
+++ Assets/Scripts/HealthBarManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.parent.rotation = Camera.main.transform.rotation;
+        // transform.parent.transform.LookAt(transform.position - Camera.main.transform.position);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.localScale = new Vector3(transform.localScale.x - 1f, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
