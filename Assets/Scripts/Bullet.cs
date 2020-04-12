c0901f7464f927f6387efb792e1f6f403511d909
tannercyost <42818307+tannercyost@users.noreply.github.com>
2020-04-12 15:49:37 -0700

    Implemented locations for defenses

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
-  public float speed = 5;
-  public float lifeTime = 5;
-  private Rigidbody rb;
-  [SerializeField] private float damage = 20;
-  private WeaponFSM _myWeapon; //keep reference to pass for book keeping if enemy dies
+    public float speed = 5;
+    public float lifeTime = 2;
+    private Rigidbody rb;
+    [SerializeField] private float damage = 20;
+    private WeaponFSM _myWeapon; //keep reference to pass for book keeping if enemy dies
 
-  public float Damage
-  {
-    get { return damage; }
-  }
+    public float Damage
+    {
+        get { return damage; }
+    }
   
-  void Awake()
-  {
-    rb = GetComponent<Rigidbody>(); 
-    rb.velocity = transform.TransformDirection(new Vector3(0,0, speed)); // sets velocity of bullet
-  }
-  void FixedUpdate()
-  {
-    lifeTime -= Time.deltaTime;
-    if (lifeTime < 0) Destroy(gameObject);
-  }
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>(); 
+        rb.velocity = transform.TransformDirection(new Vector3(0,0, speed)); // sets velocity of bullet
+    }
+    void FixedUpdate()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime < 0) Destroy(gameObject);
+    }
 }
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
-  [SerializeField] private GameObject bullet;
-  void Bang()
-  {
-    //Debug.Log("Pow");
-    Bullet shotJustFired = Instantiate(bullet, transform.position, transform.parent.rotation).GetComponent<Bullet>();
-    //shotJustFired.myWeapon = transform.parent.GetComponent<WeaponFSM>();
-  }
+    [SerializeField] private GameObject bullet;
+    void Fire()
+    {
+        //Debug.Log("Pow");
+        Vector3 spawnPos = new Vector3(transform.position.x, -0.4895477f, transform.position.z);
+        
+        Bullet shotJustFired = Instantiate(bullet, transform.position, transform.parent.rotation).GetComponent<Bullet>();
+        //shotJustFired.myWeapon = transform.parent.GetComponent<WeaponFSM>();
+    }
 }
